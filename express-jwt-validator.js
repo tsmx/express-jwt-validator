e0199b4408d2ff71c557fd41a2abd0b9288d6764
tsmx <dev@tsmx.net>
2022-04-19 21:28:27 +0200

    Configuration and default values added for flexibility

--- express-jwt-validator.js
+++ express-jwt-validator.js
@@ -1,32 +1,56 @@
-module.exports.verifyToken = function (req, res, next) {
-    const authorizationHeader = req.headers['authorization'];
-    if (typeof authorizationHeader !== 'undefined') {
-        const bearer = authorizationHeader.split(' ');
-        if (bearer.length < 2) {
-            logger.warn('Bearer token was not sent. Denying request.');
-            res.sendStatus(401);
-        }
-        const bearerToken = bearer[1];
-        jwt.verify(bearerToken, conf.tokensecret, (err, authData) => {
-            if (err) {
-                if (err.name == 'TokenExpiredError') {
-                    // TokenExpiredError should explicitly be sent to the client to 
-                    // have the ability of re-login!
-                    logger.warn('Expired bearer token was sent. Denying request.');
-                    res.status(401).json({ error: err.name });
-                }
-                else {
-                    logger.error('Invalid bearer token was sent. Denying request.');
-                    res.sendStatus(401);
-                }
-            } else {
-                req.authData = authData;
-                logger.info('Successful token verification for user: ' + authData.email + ' (client: ' + authData.client + ')');
-                next();
+const jwt = require('jsonwebtoken');
+
+const defaultHeader = 'authorization';
+const defaultFailedStatus = 401;
+const defaultSendExpiredMessage = true;
+const defaultRequestAuthProp = 'authData';
+
+module.exports.verifyToken = (conf) => {
+    // check if conf and conf.secret are present (minimal requirement)
+    if (!conf || !conf.secret) {
+        throw new Error('No secret value provided!');
+    }
+    // check for optional conf values or use default values instead
+    const authHeader = conf.header ? conf.header : defaultHeader;
+    const failedStatus = conf.failedStatus ? conf.failedStatus : defaultFailedStatus;
+    const sendExpiredMessage = conf.sendExpiredMessage ? conf.sendExpiredMessage === true : defaultSendExpiredMessage;
+    const requestAuthProp = conf.requestAuthProp ? conf.requestAuthProp : defaultRequestAuthProp;
+    // return middleware function
+    return (req, res, next) => {
+        const authorizationHeader = req.headers[authHeader];
+        if (typeof authorizationHeader !== 'undefined') {
+            const bearer = authorizationHeader.split(' ');
+            if (bearer.length < 2) {
+                if (conf.logger) conf.logger.warn('Bearer token was not sent. Denying request.');
+                res.sendStatus(failedStatus);
             }
-        });
-    } else {
-        logger.warn('AuthorizationHeader was not sent. Denying request.');
-        res.sendStatus(401);
+            const bearerToken = bearer[1];
+            jwt.verify(bearerToken, conf.secret, (err, authData) => {
+                if (err) {
+                    if (err.name == 'TokenExpiredError') {
+                        if (conf.logger) conf.logger.warn('Expired bearer token was sent. Denying request.');
+                        if (sendExpiredMessage) {
+                            // Deny TokenExpiredError with additional message payload for the ability of client re-login
+                            res.status(failedStatus).json({ error: err.name });
+                        }
+                        else {
+                            // Deny expired token without additional message
+                            res.sendStatus(failedStatus);
+                        }
+                    }
+                    else {
+                        if (conf.logger) conf.logger.error('Invalid bearer token was sent. Denying request.');
+                        res.sendStatus(failedStatus);
+                    }
+                } else {
+                    req[requestAuthProp] = authData;
+                    if (conf.logger) conf.logger.info('Token verification successful');
+                    next();
+                }
+            });
+        } else {
+            if (conf.logger) conf.logger.warn('Authorization header was not sent. Denying request.');
+            res.sendStatus(failedStatus);
+        }
     }
 };
