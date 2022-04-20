159c820bfd1bcbe16abd7e3a0fbb7bdb780f5c8c
tsmx <dev@tsmx.net>
2022-04-20 22:26:46 +0200

    Winston logger tests completed

--- test/express-jwt-validator.test.js
+++ test/express-jwt-validator.test.js
@@ -60,6 +60,16 @@ describe('express-jwt-validator test suite', () => {
         expect(memStream.toString()).toBe('');
     });
 
+    it('tests a failed access to a secret route with an invalid bearer token', async () => {
+        const app = testApp({ secret: testSecret });
+        const request = supertest(app);
+        const response = await request
+            .get('/secret')
+            .set('Authorization', 'Bearer xyz');
+        expect(response.status).toBe(401);
+        expect(memStream.toString()).toBe('');
+    });
+
     it('tests a successful access to a secret route', async () => {
         const app = testApp({ secret: testSecret });
         const request = supertest(app);
@@ -104,6 +114,46 @@ describe('express-jwt-validator test suite', () => {
         expect(response.status).toBe(401);
         expect(response.body.error).toBeUndefined();
         expect(memStream.toString()).toBe('');
+    });
+
+    it('tests a failed access to a secret route without bearer token and a winston logger defined', async () => {
+        const app = testApp({ secret: testSecret, logger: winstonLogger });
+        const request = supertest(app);
+        const response = await request
+            .get('/secret');
+        expect(response.status).toBe(401);
+        expect(memStream.toString()).toMatch(/^WARN?/);
+    });
+
+    it('tests a failed access to a secret route with an invalid auth header (\'Bearer \' prefix missing) and a winston logger defined', async () => {
+        const app = testApp({ secret: testSecret, logger: winstonLogger });
+        const request = supertest(app);
+        const response = await request
+            .get('/secret')
+            .set('Authorization', testToken);
+        expect(response.status).toBe(401);
+        expect(memStream.toString()).toMatch(/^WARN?/);
+    });
+
+    it('tests a failed access to a secret route with an invalid bearer token', async () => {
+        const app = testApp({ secret: testSecret, logger: winstonLogger });
+        const request = supertest(app);
+        const response = await request
+            .get('/secret')
+            .set('Authorization', 'Bearer xyz');
+        expect(response.status).toBe(401);
+        expect(memStream.toString()).toMatch(/^ERROR?/);
+    });
+
+    it('tests a failed access to a secret route with an expired token and a winston logger defined', async () => {
+        const app = testApp({ secret: testSecret, logger: winstonLogger });
+        const request = supertest(app);
+        const response = await request
+            .get('/secret')
+            .set('Authorization', 'Bearer ' + expiredTestToken);
+        expect(response.status).toBe(401);
+        expect(response.body.error).toBeDefined();
+        expect(memStream.toString()).toMatch(/^WARN?/);
     });
 
     it('tests a successful access to a secret route with a winston logger defined', async () => {
