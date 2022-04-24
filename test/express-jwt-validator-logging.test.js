d9a0d2c6970073c8b7a1f175fac375e02b2068f7
tsmx <dev@tsmx.net>
2022-04-24 15:56:36 +0200

    Logging tests separated into own test suite

---
+++ test/express-jwt-validator-logging.test.js
@@ -0,0 +1,80 @@
+const testApp = require('./test-app');
+const supertest = require('supertest');
+const jwt = require('jsonwebtoken');
+const winston = require('winston');
+const MemoryStream = require('memorystream');
+
+describe('express-jwt-validator logging test suite', () => {
+
+    const testSecret = '123456';
+    let testToken, expiredTestToken, winstonLogger, memStream;
+
+    let format = winston.format;
+    const myFormat = format.printf((info) => {
+        return `${info.level.toUpperCase()} ${info.message}`;
+    });
+
+    beforeEach(() => {
+        jest.resetModules();
+        testToken = jwt.sign({ user: 'TestUser123', info: 'test test' }, testSecret);
+        expiredTestToken = jwt.sign({ user: 'TestUser123', info: 'test test' }, testSecret, { expiresIn: 0 });
+        memStream = new MemoryStream(null, { readable: false, end: false })
+        winstonLogger = winston.createLogger({
+            format: myFormat,
+            transports: [new winston.transports.Stream({ stream: memStream })]
+        });
+    });
+
+    it('tests a failed access to a secret route without bearer token and a winston logger defined', async () => {
+        const app = testApp({ secret: testSecret, logger: winstonLogger });
+        const request = supertest(app);
+        const response = await request
+            .get('/secret');
+        expect(response.status).toBe(401);
+        expect(memStream.toString()).toMatch(/^WARN?/);
+    });
+
+    it('tests a failed access to a secret route with an invalid auth header (\'Bearer \' prefix missing) and a winston logger defined', async () => {
+        const app = testApp({ secret: testSecret, logger: winstonLogger });
+        const request = supertest(app);
+        const response = await request
+            .get('/secret')
+            .set('Authorization', testToken);
+        expect(response.status).toBe(401);
+        expect(memStream.toString()).toMatch(/^WARN?/);
+    });
+
+    it('tests a failed access to a secret route with an invalid bearer token and a winston logger defined', async () => {
+        const app = testApp({ secret: testSecret, logger: winstonLogger });
+        const request = supertest(app);
+        const response = await request
+            .get('/secret')
+            .set('Authorization', 'Bearer xyz');
+        expect(response.status).toBe(401);
+        expect(memStream.toString()).toMatch(/^ERROR?/);
+    });
+
+    it('tests a failed access to a secret route with an expired token and a winston logger defined', async () => {
+        const app = testApp({ secret: testSecret, logger: winstonLogger });
+        const request = supertest(app);
+        const response = await request
+            .get('/secret')
+            .set('Authorization', 'Bearer ' + expiredTestToken);
+        expect(response.status).toBe(401);
+        expect(response.body.error).toBeDefined();
+        expect(memStream.toString()).toMatch(/^WARN?/);
+    });
+
+    it('tests a successful access to a secret route with a winston logger defined', async () => {
+        const app = testApp({ secret: testSecret, logger: winstonLogger });
+        const request = supertest(app);
+        const response = await request
+            .get('/secret')
+            .set('Authorization', 'Bearer ' + testToken);
+        expect(response.status).toBe(200);
+        expect(response.body.authData.user).toBe('TestUser123');
+        expect(response.body.authData.info).toBe('test test');
+        expect(memStream.toString()).toMatch(/^INFO?/);
+    });
+
+});
--- test/express-jwt-validator.test.js
+++ test/express-jwt-validator.test.js
@@ -1,30 +1,16 @@
 const testApp = require('./test-app');
 const supertest = require('supertest');
 const jwt = require('jsonwebtoken');
-const winston = require('winston');
-const MemoryStream = require('memorystream');
 
 describe('express-jwt-validator test suite', () => {
 
     const testSecret = '123456';
-    let testToken, expiredTestToken, winstonLogger, memStream;
-
-
-    let format = winston.format;
-    const myFormat = format.printf((info) => {
-        return `${info.level.toUpperCase()} ${info.message}`;
-    });
-
+    let testToken, expiredTestToken;
 
     beforeEach(() => {
         jest.resetModules();
         testToken = jwt.sign({ user: 'TestUser123', info: 'test test' }, testSecret);
         expiredTestToken = jwt.sign({ user: 'TestUser123', info: 'test test' }, testSecret, { expiresIn: 0 });
-        memStream = new MemoryStream(null, { readable: false, end: false })
-        winstonLogger = winston.createLogger({
-            format: myFormat,
-            transports: [new winston.transports.Stream({ stream: memStream })]
-        });
     });
 
     it('tests a failed middleware construction because of a missing JWT secret', async () => {
@@ -38,7 +24,6 @@ describe('express-jwt-validator test suite', () => {
             .get('/public');
         expect(response.status).toBe(200);
         expect(response.body.path).toBe('public');
-        expect(memStream.toString()).toBe('');
     });
 
     it('tests a failed access to a secret route without bearer token', async () => {
@@ -47,7 +32,6 @@ describe('express-jwt-validator test suite', () => {
         const response = await request
             .get('/secret');
         expect(response.status).toBe(401);
-        expect(memStream.toString()).toBe('');
     });
 
     it('tests a failed access to a secret route with an invalid auth header (\'Bearer \' prefix missing)', async () => {
@@ -56,8 +40,7 @@ describe('express-jwt-validator test suite', () => {
         const response = await request
             .get('/secret')
             .set('Authorization', testToken);
-        expect(response.status).toBe(401);
-        expect(memStream.toString()).toBe('');
+        expect(response.status).toBe(401); 
     });
 
     it('tests a failed access to a secret route with an invalid bearer token', async () => {
@@ -67,7 +50,6 @@ describe('express-jwt-validator test suite', () => {
             .get('/secret')
             .set('Authorization', 'Bearer xyz');
         expect(response.status).toBe(401);
-        expect(memStream.toString()).toBe('');
     });
 
     it('tests a successful access to a secret route', async () => {
@@ -79,7 +61,6 @@ describe('express-jwt-validator test suite', () => {
         expect(response.status).toBe(200);
         expect(response.body.authData.user).toBe('TestUser123');
         expect(response.body.authData.info).toBe('test test');
-        expect(memStream.toString()).toBe('');
     });
 
     it('tests a successful access to a secret route with a custom request property', async () => {
@@ -91,7 +72,6 @@ describe('express-jwt-validator test suite', () => {
         expect(response.status).toBe(200);
         expect(response.body.tokenPayload.user).toBe('TestUser123');
         expect(response.body.tokenPayload.info).toBe('test test');
-        expect(memStream.toString()).toBe('');
     });
 
     it('tests a successful access to a secret route with a custom header property', async () => {
@@ -103,7 +83,6 @@ describe('express-jwt-validator test suite', () => {
         expect(response.status).toBe(200);
         expect(response.body.authData.user).toBe('TestUser123');
         expect(response.body.authData.info).toBe('test test');
-        expect(memStream.toString()).toBe('');
     });
 
     it('tests a failed access to a secret route with an expired token', async () => {
@@ -114,7 +93,6 @@ describe('express-jwt-validator test suite', () => {
             .set('Authorization', 'Bearer ' + expiredTestToken);
         expect(response.status).toBe(401);
         expect(response.body.error).toBeDefined();
-        expect(memStream.toString()).toBe('');
     });
 
     it('tests a failed access to a secret route with an expired token and no error message', async () => {
@@ -125,59 +103,6 @@ describe('express-jwt-validator test suite', () => {
             .set('Authorization', 'Bearer ' + expiredTestToken);
         expect(response.status).toBe(401);
         expect(response.body.error).toBeUndefined();
-        expect(memStream.toString()).toBe('');
-    });
-
-    it('tests a failed access to a secret route without bearer token and a winston logger defined', async () => {
-        const app = testApp({ secret: testSecret, logger: winstonLogger });
-        const request = supertest(app);
-        const response = await request
-            .get('/secret');
-        expect(response.status).toBe(401);
-        expect(memStream.toString()).toMatch(/^WARN?/);
-    });
-
-    it('tests a failed access to a secret route with an invalid auth header (\'Bearer \' prefix missing) and a winston logger defined', async () => {
-        const app = testApp({ secret: testSecret, logger: winstonLogger });
-        const request = supertest(app);
-        const response = await request
-            .get('/secret')
-            .set('Authorization', testToken);
-        expect(response.status).toBe(401);
-        expect(memStream.toString()).toMatch(/^WARN?/);
-    });
-
-    it('tests a failed access to a secret route with an invalid bearer token and a winston logger defined', async () => {
-        const app = testApp({ secret: testSecret, logger: winstonLogger });
-        const request = supertest(app);
-        const response = await request
-            .get('/secret')
-            .set('Authorization', 'Bearer xyz');
-        expect(response.status).toBe(401);
-        expect(memStream.toString()).toMatch(/^ERROR?/);
-    });
-
-    it('tests a failed access to a secret route with an expired token and a winston logger defined', async () => {
-        const app = testApp({ secret: testSecret, logger: winstonLogger });
-        const request = supertest(app);
-        const response = await request
-            .get('/secret')
-            .set('Authorization', 'Bearer ' + expiredTestToken);
-        expect(response.status).toBe(401);
-        expect(response.body.error).toBeDefined();
-        expect(memStream.toString()).toMatch(/^WARN?/);
-    });
-
-    it('tests a successful access to a secret route with a winston logger defined', async () => {
-        const app = testApp({ secret: testSecret, logger: winstonLogger });
-        const request = supertest(app);
-        const response = await request
-            .get('/secret')
-            .set('Authorization', 'Bearer ' + testToken);
-        expect(response.status).toBe(200);
-        expect(response.body.authData.user).toBe('TestUser123');
-        expect(response.body.authData.info).toBe('test test');
-        expect(memStream.toString()).toMatch(/^INFO?/);
     });
 
     it('tests a failed access to a secret route without bearer token and an alternative HTTP status code', async () => {
@@ -186,7 +111,6 @@ describe('express-jwt-validator test suite', () => {
         const response = await request
             .get('/secret');
         expect(response.status).toBe(403);
-        expect(memStream.toString()).toBe('');
     });
 
     it('tests a failed access to a secret route with an invalid auth header (\'Bearer \' prefix missing) and an alternative HTTP status code', async () => {
@@ -196,7 +120,6 @@ describe('express-jwt-validator test suite', () => {
             .get('/secret')
             .set('Authorization', testToken);
         expect(response.status).toBe(403);
-        expect(memStream.toString()).toBe('');
     });
 
     it('tests a failed access to a secret route with an invalid bearer token and an alternative HTTP status code', async () => {
@@ -206,7 +129,6 @@ describe('express-jwt-validator test suite', () => {
             .get('/secret')
             .set('Authorization', 'Bearer xyz');
         expect(response.status).toBe(403);
-        expect(memStream.toString()).toBe('');
     });
 
     it('tests a failed access to a secret route with an expired token and an alternative HTTP status code', async () => {
@@ -217,6 +139,5 @@ describe('express-jwt-validator test suite', () => {
             .set('Authorization', 'Bearer ' + expiredTestToken);
         expect(response.status).toBe(403);
         expect(response.body.error).toBeDefined();
-        expect(memStream.toString()).toBe('');
     });
 });
